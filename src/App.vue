3257de3e5adf4301891c5ea103b88773d6865eea
wike <200569525@qq.com>
2018-05-07 13:05:15 +0800

    日期插件

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
      <h1>WIKE的前端组建系统</h1>
      <div>
-       <a href="/datetime">日期插件</a>
+       <a href="#/datetime">日期插件</a>
      </div>
      <router-view></router-view>
   </div>
